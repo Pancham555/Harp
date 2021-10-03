99b094907f7daeedc2f107f3b1df3dc67ffc5a08
Pancham <panchamb63@gmail.com>
2021-10-03 14:44:15 +0530

    tried to add google analytics and adsense

--- client/harp/pages/_app.js
+++ client/harp/pages/_app.js
@@ -1,8 +1,45 @@
-// import '../styles/globals.css'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
 import 'tailwindcss/tailwind.css'
 import { AnimateSharedLayout } from 'framer-motion'
-function MyApp({ Component, pageProps }) {
-  return <AnimateSharedLayout><Component {...pageProps} /></AnimateSharedLayout>
+
+const App = ({ Component, pageProps }) => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
+  return (
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      ></script>
+      <script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      ></script>
+      <AnimateSharedLayout><Component {...pageProps} /></AnimateSharedLayout>
+    </>
+  )
 }
 
-export default MyApp
+export default App
--- client/harp/pages/_document.js
+++ client/harp/pages/_document.js
@@ -14,6 +14,11 @@ class MyDocument extends Document {
                     <meta httpEquiv="Content-Type" content="text/html; charset=ISO-8859-1"></meta>
                     <meta name="description" content="This is a social media site"></meta>
                     <meta name="google" content="notranslate" />
+                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2026631357019919"
+                        crossorigin="anonymous"></script>
+                    <script async custom-element="amp-auto-ads"
+                        src="https://cdn.ampproject.org/v0/amp-auto-ads-0.1.js">
+                    </script>
                     {/* <link rel="canonical" href="https://mylink" /> */}
                 </Head>
                 <body>
