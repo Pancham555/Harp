161046fef43436dcb87047de9ade1a41838e3dbf
Pancham <panchamb63@gmail.com>
2021-10-15 20:49:13 +0530

    Solved a mistake

--- Database/connection.js
+++ Database/connection.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 //Set up default mongoose connection
-var mongoDB = 'mongodb://127.0.0.1/panchamdb'
-// process.env.DBLINK || 
+var mongoDB = process.env.DBLINK
+// process.env.DBLINK ||'mongodb://127.0.0.1/panchamdb'
 
 mongoose.connect(mongoDB).then(() => console.log("DB is connected!"))
     .catch(err => console.log(err));
