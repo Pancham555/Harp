c050b7c6fd0891aa0b409af25489a28c0ba5013a
Pancham <panchamb63@gmail.com>
2021-10-18 12:07:14 +0530

    Made the feedback part

--- Database/connection.js
+++ Database/connection.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 //Set up default mongoose connection
-var mongoDB = 'mongodb://127.0.0.1/panchamdb'
+var mongoDB = process.env.DBLINK
 // process.env.DBLINK ||'mongodb://127.0.0.1/panchamdb'
 
 mongoose.connect(mongoDB).then(() => console.log("DB is connected!"))
